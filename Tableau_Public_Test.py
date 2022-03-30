fddda9b88956a52f260c748f8013845b0df699df
wanci-he <94173178+wanci-he@users.noreply.github.com>
2022-03-30 23:11:33 +0100

    Update Tableau_Public_Test.py

--- Tableau_Public_Test.py
+++ Tableau_Public_Test.py
@@ -19,9 +19,8 @@ def sidebar_info():
 #the body of the page
 def main():
 
-     
-    st.markdown(f'# Who Becomes an Inventor in America? Important factors nurturing future inventors
-    ')
+    st.title('Who Becomes an Inventor in America? Important factors nurturing future inventors')
+    
 
     html_temp = """
     <div class='tableauPlaceholder' id='viz1648677778419' style='position: relative'><noscript><a href='#'><img alt='仪表板 1 ' src='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;A2&#47;A2_eudcation&#47;1&#47;1_rss.png' style='border: none' /></a></noscript><object class='tableauViz'  style='display:none;'><param name='host_url' value='https%3A%2F%2Fpublic.tableau.com%2F' /> <param name='embed_code_version' value='3' /> <param name='site_root' value='' /><param name='name' value='A2_eudcation&#47;1' /><param name='tabs' value='no' /><param name='toolbar' value='yes' /><param name='static_image' value='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;A2&#47;A2_eudcation&#47;1&#47;1.png' /> <param name='animate_transition' value='yes' /><param name='display_static_image' value='yes' /><param name='display_spinner' value='yes' /><param name='display_overlay' value='yes' /><param name='display_count' value='yes' /><param name='language' value='en-GB' /><param name='filter' value='publish=yes' /></object></div>                <script type='text/javascript'>                    var divElement = document.getElementById('viz1648677778419');                    var vizElement = divElement.getElementsByTagName('object')[0];                    if ( divElement.offsetWidth > 800 ) { vizElement.style.width='1000px';vizElement.style.height='527px';} else if ( divElement.offsetWidth > 500 ) { vizElement.style.width='1000px';vizElement.style.height='527px';} else { vizElement.style.width='100%';vizElement.style.height='727px';}                     var scriptElement = document.createElement('script');                    scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';                    vizElement.parentNode.insertBefore(scriptElement, vizElement);                </script>
